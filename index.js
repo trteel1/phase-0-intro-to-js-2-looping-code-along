e4d715df8b1b2b123fd0e75fec629a9db0dc8c2d
trteel1 <tateelamino@gmail.com>
2022-12-09 17:18:44 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards([name1, name2, name3], event) {
+  return (writeCards = [
+    `Thank you, ${name1}, for the wonderful ${event} gift!`,
+    `Thank you, ${name2}, for the wonderful ${event} gift!`,
+    `Thank you, ${name3}, for the wonderful ${event} gift!`,
+  ]);
+
+}
+const num = 10;
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+  return num;
+}
